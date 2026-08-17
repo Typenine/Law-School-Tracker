bf78fad9fe801faf11a74a9a637f066320f74c29
Typenine <patrickmmcnulty62@yahoo.com>
2026-08-17 09:50:25 -0700

    Export GPT retrieval helpers

--- lib/aiNotes.ts
+++ lib/aiNotes.ts
@@ -2,3 +2,5 @@ export * from './notes/types';
 export * from './notes/notebooks';
 export * from './notes/sections';
 export * from './notes/pages';
+export * from './notes/navigation';
+export * from './notes/semantic';
