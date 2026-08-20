7d9c230fc74b2f8b8d1efcfa5c9bbf1e1fdaa9a0
Typenine <patrickmmcnulty62@yahoo.com>
2026-08-20 11:23:36 -0700

    Expose task timer on Tasks page

--- app/tasks/page.tsx
+++ app/tasks/page.tsx
@@ -1,5 +1,11 @@
+import TaskTimerBar from '@/components/TaskTimerBar';
 import TaskWorkspaceV2 from '@/components/TaskWorkspaceV2';
 
 export default function TasksPage() {
-  return <TaskWorkspaceV2 />;
+  return (
+    <div className="space-y-4">
+      <TaskTimerBar />
+      <TaskWorkspaceV2 />
+    </div>
+  );
 }
